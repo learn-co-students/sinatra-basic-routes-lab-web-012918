a25f87fd1427b32fb321dd3277141b40dec2ad6f
gilsunshine <gilsunshine@gmail.com>
2018-02-17 11:13:36 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Gil"
+  end
+
+  get '/hometown' do
+    "My hometown is Hastings-on-Hudson"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is My Name is Jonas"
+  end
+
 end
