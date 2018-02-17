115570310759c08abd5ef63a6f268a3d59261ccb
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-02-17 12:36:34 -0500

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Brooke"
+  end
+
+  get '/hometown' do
+    "My hometown is New York City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Scarlet Begonias"
+  end
+
+
 end
