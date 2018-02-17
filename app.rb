bec40741a92c086a2061875919a98af7c3b82c97
aalexander3 <aralx73@gmail.com>
2018-02-17 15:05:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Arren"
+  end
+
+  get '/hometown' do
+    "My hometown is Madison"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Breeze by Dr. Dog"
+  end
+
 end
